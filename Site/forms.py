b03c3ab9922e0f51e7d28bf436d3b99491fba392
Joe <jbutler5510@gmail.com>
2019-11-14 10:41:43 -0500

    fixed user profile page and nav bar

--- Site/forms.py
+++ Site/forms.py
@@ -16,22 +16,6 @@ from Site.models import User
 
 authy_api = AuthyApiClient(app.config.get('ACCOUNT_SECURITY_API_KEY'))
 
-'''class RegFrom(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Submit')
-
-    def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
-        if user:
-            raise ValidationError('Username already in use')
-
-    def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
-        if user:
-            raise ValidationError('An account with that email already exists')'''
 
 
 class RegForm(FlaskForm):
@@ -99,13 +83,6 @@ class UpdateProfileForm(FlaskForm):
                 raise ValidationError('An account with that email already exists')
 
 
-'''
-class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    rememberme = BooleanField('Remember Me')
-    submit = SubmitField('Login')
-'''
 
 
 class LoginForm(FlaskForm):
